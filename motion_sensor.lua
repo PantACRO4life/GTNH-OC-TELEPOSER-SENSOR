f816838af15fcda463777504f344807493086b1b
PantACRO4life <6643071+PantACRO4life@users.noreply.github.com>
2025-06-26 22:08:33 -0400

    Add files via upload

---
+++ motion_sensor.lua
@@ -0,0 +1,24 @@
+-- Creator :: PantACRO4life
+local component = require("component")
+local event = require("event")
+local os = require("os")
+
+--=== Configuration ===--
+local freq = 555          -- Wireless redstone frequency
+local SensorSense = 7     -- Sensitivity level of the Motion Sensor In Blocks
+local OnOff = 0.5         -- Redstone ON time in seconds
+--======================--
+
+local redstone = component.redstone
+local sensor = component.motion_sensor
+
+redstone.setWirelessFrequency(freq)
+sensor.setSensitivity(SensorSense)
+
+while true do
+  event.pull("motion")
+  redstone.setWirelessOutput(true)
+  os.sleep(OnOff)
+  redstone.setWirelessOutput(false)
+  os.sleep(0.5)  -- Debouncer
+end
